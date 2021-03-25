e87349d90e59dd323edb9d170804ac868381597e
OrionCarlos97 <carlosbustos1997@gmail.com>
2021-03-25 16:45:48 -0500

    corrige error

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@ const HtmlWebpackPlugin = require('html-webpack-plugin')
 const MiniCssExtractPlugin = require('mini-css-extract-plugin')
 const CssMinimizerPlugin  = require('css-minimizer-webpack-plugin')
 const { CleanWebpackPlugin } = require('clean-webpack-plugin')
-const TerserPlugin = reuie('terser-webpack-plugin')
+const TerserPlugin = require('terser-webpack-plugin')
 
 module.exports= {
     entry: './src/index.js', //Entrada del archivo principal
